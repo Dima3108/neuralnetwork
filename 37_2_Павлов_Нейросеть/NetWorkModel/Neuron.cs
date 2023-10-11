12c4d1153c0070a3991563f62110d864ef6212e1
Dima <73338242+Dima3108@users.noreply.github.com>
2023-10-11 15:44:00 +0300

    t

--- 37_2_Павлов_Нейросеть/NetWorkModel/Neuron.cs
+++ 37_2_Павлов_Нейросеть/NetWorkModel/Neuron.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//https://ru.wikipedia.org/wiki/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8F_%D0%B0%D0%BA%D1%82%D0%B8%D0%B2%D0%B0%D1%86%D0%B8%D0%B8
 
 namespace _37_2_Павлов_Нейросеть.NetWorkModel
 {

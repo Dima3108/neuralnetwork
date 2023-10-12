e704ec3d161a17e6f8c4cce634a82c767c387e7b
Dima <73338242+Dima3108@users.noreply.github.com>
2023-10-12 12:15:30 +0300

    Add files via upload

---
+++ 37_2_Павлов_Нейросеть/NetWorkModel/MemoryMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_2_Павлов_Нейросеть.NetWorkModel
+{
+    public enum MemoryMode
+    {
+        INIT=0,
+        SET=1,
+        GET=2
+    };
+}

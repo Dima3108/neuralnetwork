f14c56a83981ea679d39f0b9877e68fea588aae4
Dima <73338242+Dima3108@users.noreply.github.com>
2023-10-12 12:23:26 +0300

    r

---
+++ 37_2_Павлов_Нейросеть/NetWorkModel/Documents/Neuron.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://ru.wikipedia.org/wiki/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8F_%D0%B0%D0%BA%D1%82%D0%B8%D0%B2%D0%B0%D1%86%D0%B8%D0%B8
+
+namespace _37_2_Павлов_Нейросеть.NetWorkModel
+{
+    public class Neuron
+    {
+        public double Output { get; }
+        public double Derivative { get; }
+        public double[] Weights { get; set; }
+        public double[] InputData
+        {
+            get; set;
+        }
+        private TypeNeuron _type;
+
+        private double LogicFunc(double x) => 1 / (1 + Math.Pow(Math.E, -x));
+        private double LogicFuncDerivative(double x)
+        {
+            double val = LogicFunc(x);
+            return val * (1 - val);
+        }
+        public void Activator(double[] inpt, double[] wght)
+        {
+            switch (_type)
+            {
+                case TypeNeuron.HiddenNeuron:break;
+                case TypeNeuron.OutputNeuron:break;
+            }
+        }
+    }
+}
--- 37_2_Павлов_Нейросеть/NetWorkModel/Layer.cs
+++ 37_2_Павлов_Нейросеть/NetWorkModel/Layer.cs
@@ -9,11 +9,56 @@ namespace _37_2_Павлов_Нейросеть.NetWorkModel
 {
    public abstract class Layer
     {
-        public Layer(Layer _n = null)
+        protected Layer(int non,int nopn,TypeNeuron _type,string nameLayer,Layer _n = null)
         {
             _nextLayer = _n;
+            neurons = new Neuron[non];
+            numofprevneurons = nopn;
+            fLauerName = nameLayer;
+            pathDirWeights = AppDomain.CurrentDomain.BaseDirectory;
+            double[,] weights;//временныймассив весов
+            weights = null; 
+            lastdeltaweights = new double[non, nopn + 1];
+            for(int i = 0; i < non; ++i)
+            {
+                double[] tmp_weights = new double[nopn + 1];
+                for(int j = 0; j < nopn + 1; ++j)
+                {
+                    tmp_weights[j] = weights[i, j];
+                }
+                neurons[i] = new Neuron(tmp_weights, _type);
+            }
+
+
+        }
+        public double[,]WeightInitialize(MemoryMode mm,string path)
+        {
+            switch (mm)
+            {
+                case MemoryMode.INIT: return null;
+                default:return null;
+            }
         }
        private  protected Neuron[] neurons;
+        public Neuron[] Neurons
+        {
+            get => neurons;
+            set=>neurons = value;
+        }
+        public double[] Data
+        {
+            set
+            {
+               for(int i = 0; i < Neurons.Length; i++)
+                {
+ Neurons[i].InputData=value;
+ Neurons[i].Activator(Neurons[i].InputData,Neurons[i].Weights);
+                }
+                 
+                 
+                 
+            }
+        }
         /// <summary>
         /// Внутренний уровень
         /// </summary>
@@ -26,6 +71,7 @@ namespace _37_2_Павлов_Нейросеть.NetWorkModel
         /// путь к катологу
         /// </summary>
         string pathDirWeights;
+        string pathFileWeights;
         protected int numofneurons { get => neurons.Length; }
         /// <summary>
         /// кол-во пред нейронов
@@ -39,5 +85,6 @@ namespace _37_2_Павлов_Нейросеть.NetWorkModel
         /// Скорость обучения
         /// </summary>
         protected const double learmingrate = 0.5;
+        protected const double momentum=0.05d;
     }
 }

b8228347eba3ccb3e979c4c83e123f6527da27a9
Dima <73338242+Dima3108@users.noreply.github.com>
2023-10-12 11:41:25 +0300

    Add files via upload

--- 37_2_Павлов_Нейросеть/NetWorkModel/Layer.cs
+++ 37_2_Павлов_Нейросеть/NetWorkModel/Layer.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
+using System.Windows.Forms;
 namespace _37_2_Павлов_Нейросеть.NetWorkModel
 {
    public abstract class Layer
@@ -17,5 +18,26 @@ namespace _37_2_Павлов_Нейросеть.NetWorkModel
         /// Внутренний уровень
         /// </summary>
         private protected Layer _nextLayer;
+        /// <summary>
+        /// имя слоя
+        /// </summary>
+         protected string fLauerName { get; }
+        /// <summary>
+        /// путь к катологу
+        /// </summary>
+        string pathDirWeights;
+        protected int numofneurons { get => neurons.Length; }
+        /// <summary>
+        /// кол-во пред нейронов
+        /// </summary>
+        protected int numofprevneurons;
+        /// <summary>
+        /// массив весов предыдущих слоев
+        /// </summary>
+        protected double[,] lastdeltaweights;
+        /// <summary>
+        /// Скорость обучения
+        /// </summary>
+        protected const double learmingrate = 0.5;
     }
 }

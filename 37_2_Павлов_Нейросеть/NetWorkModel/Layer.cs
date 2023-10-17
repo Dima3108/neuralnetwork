278d4c4897a4f8f54a8b06ea6236a6666924fd46
Dima <73338242+Dima3108@users.noreply.github.com>
2023-10-17 12:05:24 +0300

    init weght

--- 37_2_Павлов_Нейросеть/NetWorkModel/Layer.cs
+++ 37_2_Павлов_Нейросеть/NetWorkModel/Layer.cs
@@ -7,33 +7,41 @@ using System.IO;
 using System.Windows.Forms;
 namespace _37_2_Павлов_Нейросеть.NetWorkModel
 {
-   public abstract class Layer
+    public abstract class Layer
     {
-        protected Layer(int non,int nopn,TypeNeuron _type,string nameLayer,Layer _n = null)
+        protected Layer(int non, int nopn, TypeNeuron _type, string nameLayer, Layer _n = null)
         {
             _nextLayer = _n;
             neurons = new Neuron[non];
             numofprevneurons = nopn;
             fLauerName = nameLayer;
             pathDirWeights = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "memory");
-            if(!Directory.Exists(pathDirWeights))
+            if (!Directory.Exists(pathDirWeights))
                 Directory.CreateDirectory(pathDirWeights);
-                // AppDomain.CurrentDomain.BaseDirectory;
-            pathFileWeights =Path.Combine( pathDirWeights,nameLayer+".csv");
+            // AppDomain.CurrentDomain.BaseDirectory;
+            pathFileWeights = Path.Combine(pathDirWeights, nameLayer + ".csv");
 #if DEBUG
             File.Create(pathFileWeights);
             Console.WriteLine(pathFileWeights);
 #endif
-            double[,] weights;//временныймассив весов
+            double[,] weights;//временный массив весов
+
             weights = null;
-#if DEBUG
-            weights = WeightInitialize(MemoryMode.INIT, pathFileWeights);
-#endif
+            /*#if DEBUG
+                        weights = WeightInitialize(MemoryMode.INIT, pathFileWeights);
+            #endif*/
+            if (File.Exists(pathFileWeights))
+                weights = WeightInitialize(MemoryMode.GET, pathFileWeights);
+            else
+            {
+                weights = WeightInitialize(MemoryMode.INIT, pathFileWeights);
+
+            }
             lastdeltaweights = new double[non, nopn + 1];
-            for(int i = 0; i < non; ++i)
+            for (int i = 0; i < non; ++i)
             {
                 double[] tmp_weights = new double[nopn + 1];
-                for(int j = 0; j < nopn + 1; ++j)
+                for (int j = 0; j < nopn + 1; ++j)
                 {
                     tmp_weights[j] = weights[i, j];
                 }
@@ -42,37 +50,64 @@ namespace _37_2_Павлов_Нейросеть.NetWorkModel
 
 
         }
-        public double[,]WeightInitialize(MemoryMode mm,string path)
-        { double[,] w = new double[numofneurons, numofprevneurons+1];
+        public double[,] WeightInitialize(MemoryMode mm, string path)
+        {
+            double[,] w = new double[numofneurons, numofprevneurons + 1];
             switch (mm)
             {
                 case MemoryMode.INIT:
-                   
-
-                    return w;
+                    Random random = new Random();
+                    double ts = 0;
+                    for (int i = 0; i < numofneurons; i++)
+                    {
+                        for (int j = 0; j < numofprevneurons + 1; j++)
+                        {
+                            w[i, j] = 2 * random.NextDouble() - 1;
+                            ts += w[i, j];
+                        }
+                    }
+                    ts /= (double)((numofprevneurons + 1) * numofneurons);
+                    for (int i = 0; i < numofneurons; i++)
+                        for (int j = 0; j < numofprevneurons + 1; j++)
+                            w[i, j] -= ts;
+                    break;
                 case MemoryMode.GET:
-                    return w;
-                default:return null;
+                    string[] lines = File.ReadAllLines(path);
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        string[] val = lines[i].Split(' ');
+                        for (int j = 0; j < val.Length; j++)
+                            w[i, j] = double.Parse(val[j]);
+                    }
+                    break;
+                case MemoryMode.SET:
+                    //Здесь другая функция
+                    break;
+                    // return w;
+                    /* case MemoryMode.GET:
+                         return w;
+                     default:return null;*/
             }
+            return w;
         }
-       private  protected Neuron[] neurons;
+        private protected Neuron[] neurons;
         public Neuron[] Neurons
         {
             get => neurons;
-            set=>neurons = value;
+            set => neurons = value;
         }
         public double[] Data
         {
             set
             {
-               for(int i = 0; i < Neurons.Length; i++)
+                for (int i = 0; i < Neurons.Length; i++)
                 {
- Neurons[i].InputData=value;
- Neurons[i].Activator(Neurons[i].InputData,Neurons[i].weights);
+                    Neurons[i].InputData = value;
+                    Neurons[i].Activator(Neurons[i].InputData, Neurons[i].weights);
                 }
-                 
-                 
-                 
+
+
+
             }
         }
         /// <summary>
@@ -82,7 +117,7 @@ namespace _37_2_Павлов_Нейросеть.NetWorkModel
         /// <summary>
         /// имя слоя
         /// </summary>
-         protected string fLauerName { get; }
+        protected string fLauerName { get; }
         /// <summary>
         /// путь к катологу
         /// </summary>
@@ -97,10 +132,11 @@ namespace _37_2_Павлов_Нейросеть.NetWorkModel
         /// массив весов предыдущих слоев
         /// </summary>
         protected double[,] lastdeltaweights;
+
         /// <summary>
         /// Скорость обучения
         /// </summary>
         protected const double learmingrate = 0.5;
-        protected const double momentum=0.05d;
+        protected const double momentum = 0.05d;
     }
 }

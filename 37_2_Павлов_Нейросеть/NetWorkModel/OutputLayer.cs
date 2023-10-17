b39eb417570ca5400ac327c8bc51d96254bff2cc
Dima <73338242+Dima3108@users.noreply.github.com>
2023-10-17 12:20:38 +0300

    init OutputLayer

--- 37_2_Павлов_Нейросеть/NetWorkModel/OutputLayer.cs
+++ 37_2_Павлов_Нейросеть/NetWorkModel/OutputLayer.cs
@@ -8,11 +8,20 @@ namespace _37_2_Павлов_Нейросеть.NetWorkModel
 {
     public class OutputLayer:Layer
     {
-#if DEBUG
+#if false
         public OutputLayer(Layer _n=null) : base(10, 32, TypeNeuron.OutputNeuron, nameof(OutputLayer), _n) { 
 
         }
 #endif
+        public OutputLayer(int non,int nopn,TypeNeuron tn,string name) : base(non, nopn, tn, name) { }
+        public override void Recognaize(Network network,Layer layer)
+        {
+
+        }
+        public override void BackWardPass(double[] stuff)
+        {
+            //throw new NotImplementedException();
+        }
         // public OutputLayer( int nopn, TypeNeuron _type, string nameLayer, Layer _n = null):base(10,)
         /*  {
               neurons = new Neuron[10];
